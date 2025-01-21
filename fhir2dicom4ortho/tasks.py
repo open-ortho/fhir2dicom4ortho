17eec835243a7a3daa64ddec94c1f393515b7052
Toni Magni <email@antoniomagni.com>
2025-01-21 20:15:22 +0100

    Enhance DICOM image building by validating ImagingStudy presence and extracting series/instance details

--- fhir2dicom4ortho/tasks.py
+++ fhir2dicom4ortho/tasks.py
@@ -1,5 +1,6 @@
 from fhir.resources.bundle import Bundle
 from fhir.resources.binary import Binary
+from fhir.resources.imagingstudy import ImagingStudy
 from fhir2dicom4ortho import logger, args_cache
 
 from dicom4ortho.controller import OrthodonticController
@@ -20,6 +21,7 @@ def _build_dicom_image(bundle:Bundle, task_id, task_store)-> OrthodonticPhotogra
     logger.debug("Extracting Binary resources")
     image_binary = None
     dicom_binary = None
+    imagingstudy = None
 
     for entry in bundle.entry:
         resource = entry.resource
@@ -28,10 +30,12 @@ def _build_dicom_image(bundle:Bundle, task_id, task_store)-> OrthodonticPhotogra
                 image_binary = resource
             elif resource.contentType == "application/dicom":
                 dicom_binary = resource
+        elif isinstance(resource, ImagingStudy):
+            imagingstudy = ImagingStudy.model_validate(resource)
 
-    if not image_binary or not dicom_binary:
+    if not image_binary or not dicom_binary or not imagingstudy:
         task_store.modify_task_status(task_id, TASK_REJECTED)
-        raise ValueError("Invalid Bundle: Must contain one image Binary and one DICOM Binary")
+        raise ValueError("Invalid Bundle: Must contain one image Binary, one DICOM Binary and one ImagingStudy.")
 
     logger.debug("Converting Binary resources to image and dataset")
     # image = convert_binary_to_image(image_binary)
@@ -42,14 +46,40 @@ def _build_dicom_image(bundle:Bundle, task_id, task_store)-> OrthodonticPhotogra
     scheduled_protocol_code_value = get_opor_code_value_from_code(scheduled_protocol_code)
 
     logger.debug("Building OrthodonticPhotograph")
+    try:
+        series0 = imagingstudy.series[0]
+        instance0 = series0.instance[0]
+    except (IndexError, AttributeError):
+        raise ValueError("Invalid ImagingStudy: Must contain at least one Series and one Instance.")
+
+    series0_number = None
+    if hasattr(series0, 'number'):
+        series0_number = series0.number
+
+    series0_uid = None
+    if hasattr(series0, 'uid'):
+        series0_uid = series0.uid
+
+    instance0_uid = None
+    if hasattr(instance0, 'uid'):
+        instance0_uid = instance0.uid
+
+    instance0_number = None
+    if hasattr(instance0, 'number'):
+        instance0_number = instance0.number
+
     orthodontic_photograph:OrthodonticPhotograph = OrthodonticPhotograph(
+        sop_instance_uid=instance0_uid,
         input_image_bytes=image_binary.data,
-        image_type=scheduled_protocol_code_value,
         dicom_mwl=mwl_dataset
     )
         
     logger.debug("Copying MWL tags to OrthodonticPhotograph")
     orthodontic_photograph.copy_mwl_tags(dicom_mwl=mwl_dataset)
+    orthodontic_photograph.series_instance_uid = series0_uid
+    orthodontic_photograph.series_number = series0_number
+    orthodontic_photograph.instance_number = instance0_number
+    orthodontic_photograph.set_dicom_attributes_by_type_keyword()
     orthodontic_photograph.prepare()
     return orthodontic_photograph
 
--- test/test_fhir2dicom4ortho.py
+++ test/test_fhir2dicom4ortho.py
@@ -1,3 +1,4 @@
+import os
 import test
 from fhir2dicom4ortho.task_store import TaskStore
 from fhir2dicom4ortho.tasks import _build_dicom_image, TASK_DRAFT, TASK_COMPLETED, TASK_FAILED, TASK_REJECTED, TASK_INPROGRESS
@@ -17,8 +18,13 @@ class TestTasks(unittest.TestCase):
         self.assertIsNotNone(orthodontic_photograph)
 
         ds = orthodontic_photograph.to_dataset()
+        print(ds)
+        code = orthodontic_photograph.get_scheduled_protocol_code()
+        self.assertIsNotNone(code)
+        self.assertEqual(code.CodeValue, "EV20")
+        ds.save_as(os.path.join(test.current_dir,"test_build_dicom_image.dcm"))
         self.assertIsNotNone(ds)
-        self.assertEqual(ds.InstanceNumber, "1")
+        self.assertEqual(ds.InstanceNumber, "00100")
         self.assertEqual(ds.SeriesInstanceUID, "1.3.6.1.4.1.61741.11.2.3.250.184.10.136.8.7")
         self.assertEqual(ds.SOPInstanceUID, "1.3.6.1.4.1.61741.11.2.4.146.2.192.6.158.81")
 
